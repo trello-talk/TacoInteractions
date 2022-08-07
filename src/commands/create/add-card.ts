dd80400bf860aec69b1862774d6b4c1faa42994a
Snazzah <Snazzah@users.noreply.github.com>
2022-08-07 04:14:30 +0000

    chore(lint): auto-lint source code

--- src/commands/create/add-card.ts
+++ src/commands/create/add-card.ts
@@ -33,7 +33,8 @@ export default class AddCardCommand extends SlashCommand {
 
     const [board] = await getBoard(userData.trelloToken, userData.currentBoard, userData.trelloID);
     // NOTE: we can't check per list, so this might fail anyways.
-    if (board.cards.filter((c) => !c.closed).length >= 5000 || board.cards.length >= 2000000) return { content: t('addcard.limited'), ephemeral: true };
+    if (board.cards.filter((c) => !c.closed).length >= 5000 || board.cards.length >= 2000000)
+      return { content: t('addcard.limited'), ephemeral: true };
 
     const list = board.lists.find((l) => l.id === ctx.options.list || l.name === ctx.options.list);
     if (!list) return t('query.not_found', { context: 'list' });
